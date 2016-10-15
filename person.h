c0ba1cab6f7228391feb73829ff84c36d17c1e6f
lazybing <libinglimit@gmail.com>
2016-10-15 00:44:44 -0700

    add the person class

---
+++ person.h
@@ -0,0 +1,15 @@
+class Person{
+    public:
+        Person():Name("Bing Li"), Address("Mediatek"){}
+        void Set_Name(const std::string string)
+        {Name = string;}
+        void Set_Address(const std::string string)
+        {Address = string;}
+        std::string Get_Name() const
+        {return Name;}
+        std::string Get_Address() const
+        {return Address;}
+    private:
+        std::string Name;
+        std::string Address;
+}
