7ea33ecd716bf3d733198705d6b9a3fc9776b97e
lazybing <libinglimit@gmail.com>
2019-07-05 21:15:59 -0700

    more practice for template

---
+++ leftover.cpp
@@ -0,0 +1,63 @@
+// left.cpp -- string function with a default argument
+#include <iostream>
+unsigned long left(unsigned long  num, unsigned ct);
+char *left(const char *str, int n = 1);
+
+int main()
+{
+    using namespace std;
+    char *trip = "Hawaii!!";
+    unsigned long n = 12345678;
+    int i;
+    char *temp;
+
+    for (i = 1; i < 10; i++)
+    {
+	cout << left(n, i) << endl;
+	temp = left(trip, i);
+	cout << temp << endl;
+	delete [] temp;
+    }
+    return 0;
+}
+
+// This function returns the rist ct digits of the number num.
+unsigned long left(unsigned long num, unsigned ct)
+{
+    unsigned digits = 1;
+    unsigned long n = num;
+
+    if (ct == 0 || num == 0)
+	return 0;  // return 0 if no digits
+
+    while(n /= 10)
+	digits++;
+
+    if (digits > ct)
+    {
+	ct = digits - ct;
+	while(ct--)
+	    n /= 10;
+
+	return num;
+    }else{
+	return num;
+    }
+}
+
+// This function returns a pointer to a new string
+// consisting of the first n characters in the str string.
+char * left(const char * str, int n)
+{
+    if (n < 0)
+	n = 0;
+    char *p = new char[n + 1];
+
+    int i;
+    for (i = 0; i < n && str[i]; i++)
+	p[i] = str[i];	//copy characters
+    while(i <= n)
+	p[i++] = '\0';  // set rest of string to '\0'
+
+    return p;
+}
