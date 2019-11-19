763b5eb4d6505420fe347b7aec25a5001b5b455e
lazybing <libinglimit@gmail.com>
2019-11-18 18:35:04 -0800

    print learning in python

---
+++ Python_Language_Learning/Beging_Python_3th/ch5/print.py
@@ -0,0 +1,17 @@
+# print more params
+print("Age:", 42)
+
+name = 'Gumby'
+salutation = 'Mr.'
+greeting = 'Hello,'
+print(greeting, salutation, name)
+greeting = 'Hello'
+print(greeting, ',', salutation, name)
+print(greeting + ',', salutation + name)
+
+# define the seperate str using in print
+print("I", "wish", "to", "register", "a", "complaint", sep = "_")
+
+#define the end str using in print
+print("Hello,", end="")
+print("World!")
